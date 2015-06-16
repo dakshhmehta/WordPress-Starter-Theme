f599f2410e8af16da861a569b7553562630b4392
kunalkhandol <kunalkhandol@ymail.com>
2015-06-16 06:25:14 +0530

    Redux configurations added.

--- core/init.php
+++ core/init.php
@@ -3,3 +3,5 @@
 require_once __DIR__.'/plugin-activation/class-tgm-plugin-activation.php';
 
 require_once __DIR__.'/../framework/required-plugins.php';
+
+require_once __DIR__.'/../framework/theme-options.php';
